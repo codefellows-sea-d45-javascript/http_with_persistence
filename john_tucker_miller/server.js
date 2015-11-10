c3214ba15616560737cd2db9bdbe9978e7d28b44
jhnmllr <tucker.miller@gmail.com>
2015-11-10 12:27:36 -0800

    still needs testing

---
+++ john_tucker_miller/server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
+  res.json(JSON.parse(fs.readFileSync('./data/data.json')));
+});
+
+app.post('/', function (req, res) {
+  fs.writeFileSync('./data/data.json', JSON.stringify(req.body));
+});
+
+app.listen(3000);
