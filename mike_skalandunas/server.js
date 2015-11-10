03545e2ad10ef38720b4ea2bc6bb4ac88533e5be
Mike Skalandunas <mskalandunas@gmail.com>
2015-11-10 11:43:14 -0800

    add post request

--- mike_skalandunas/server.js
+++ mike_skalandunas/server.js
@@ -9,6 +9,25 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(bodyParser.json());
 
+app.post('/data', function(req, res) {
+  res.send('got a response.');
+})
+
+app.post('/data', function(req, res, next) {
+  fs.appendFile(__dirname + 'data', JSON.stringify(req.body), function(err, data) {
+    next();
+  })
+})
+
+app.get('data', function(req, res, next) {
+  req.data = '';
+
+  fs.readFile(__dirname + '/data', function(err, data) {
+    req.data = data.toString();
+    next();
+  }
+})
+
 app.listen(app.get('port'), function() {
   console.log('Server listening at ' + app.get('port') + '.');
 })
