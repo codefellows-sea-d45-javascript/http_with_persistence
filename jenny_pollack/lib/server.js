bc6956cebf9c0ef0596a315eb1dd6b5234b93229
Jenny Pollack <JennyPollack@Jennys-MacBook-Pro.local>
2015-11-10 21:34:04 -0800

    i think it works

--- jenny_pollack/lib/server.js
+++ jenny_pollack/lib/server.js
@@ -1,13 +1,17 @@
 var app = require('express')();  
-var bodyParser = require('body-parser');
 var fs = require('fs'); 
 
-app.get('/greet/:name', function(req, res) {
-  res.json({msg: 'hell0 ' + req.params.name});
+app.get('/greet', function(req, res) {
+	fs.readFile(__dirname + '/../data/greet.json', function(err, data){
+		res.send(JSON.parse(data.toString())); 
+	});
+
 });
 
 app.post('/greet', function(req, res){
-
+	var info = fs.createWriteStream(__dirname + '/../data/greet.json');
+	req.pipe(info);  
+	res.send('written'); 
 }); 
 
 app.listen(3000, function() {
