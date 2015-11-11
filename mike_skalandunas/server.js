7503226ed46046f75f9a52cfaa6f2d77fab217bb
Mike Skalandunas <mskalandunas@gmail.com>
2015-11-10 22:02:02 -0800

    add inclass code

--- mike_skalandunas/server.js
+++ mike_skalandunas/server.js
@@ -7,30 +7,18 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(bodyParser.json());
+var processData = function(req, res, next) {
+  console.log('process data');
+  var data = '';
+  req.on('data', function(reqData) {
 
-app.post('/data', function(req, res) {
-  res.send('got a response.');
-})
-
-app.post('/data', function(req, res, next) {
-  fs.appendFile(__dirname + 'data', JSON.stringify(req.body), function(err, data) {
+    data = data + reqData.toString();
+  });
+  req.on('end', function(endData) {
+    req.body = data;
     next();
-  })
-})
-
-app.get('data', function(req, res, next) {
-  req.data = '';
-
-  fs.readFile(__dirname + '/data', function(err, data) {
-    req.data = data.toString();
-    next();
-  }
-})
-
-app.get('/data', function(req, res) {
-  res.send(req.data);
-})
+  });
+};
 
 app.listen(app.get('port'), function() {
   console.log('Server listening at ' + app.get('port') + '.');
