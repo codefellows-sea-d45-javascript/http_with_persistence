03e0ccc0f5fcc077a3327d396658e4bfd6ffe47f
Michael Tse <michael.tse3@gmail.com>
2015-11-10 17:22:53 -0800

    post request works and can get json file to display on page

--- michael_tse/server.js
+++ michael_tse/server.js
@@ -3,12 +3,6 @@ var app = express();
 var http = require('http');
 var fs = require('fs');
 
-
-
-// It should respond to GET/POST requests for a single route
-// and the data coming in from a post request should be saved to a json file in
-// a data folder in your repository.
-
 var processData = function(req, res, next) {
   console.log('process data');
   var data = '';
@@ -23,47 +17,13 @@ var processData = function(req, res, next) {
 
 app.use(processData);
 
+//takes in a post request and stores newly made json file in data directory.
 app.post('/:name', function(req, res) {
-  console.log('post me!')
-  fs.writeFile(__dirname + '/data/' + req.params.name + '.json', function(err) {
-    if(err) throw err;
+  fs.writeFileSync(__dirname + '/data/' + req.params.name + '.json', req.body);
     res.send(req.body);
-    console.log('sucess post!');
-  });
-  res.send('data recieved');
+    //console log confirms to server file was posted
+    console.log('posted new file: ' + req.params.name + '.json');
 });
-
-
-
-// app.get('/:name', function(req, res, next) {
-//   // var data = {}
-//   var data = fs.readFileSync(__dirname + '/data/' + req.params.name + '.json')
-//   res.send(JSON.parse(data));
-
-//   // fs.readFile(__dirname + '/data/' + req.params.name + '.json', function(err, data) {
-//   //   if(err) throw err;
-//   //   data = ;
-//   // });
-//   next();
-// });
-
-
-
-
-
-
-
-// app.post('/:name', function(req, res) {
-//   console.log('store that data!');
-//   res.send()
-// })
-
-
-// if a request is sent to "/note_one" with a body of {noteBody: 'hello world'}
-// the json data in the body should be stored in it's own json file `data/note_one.json`.
-
-
-
 
 // A get request to the same route should return the data contained in the json file.
 app.get('/:name', function(req, res) {
--- michael_tse/tests/tests.js
+++ michael_tse/tests/tests.js
@@ -9,7 +9,17 @@ describe('The server file', function() {
 //   before(function() {
 //     this.indexFileString = fs.readFileSync(__dirname + '/../data/sampleJSON').toString();
 //   });
-// J
+
+  it('should be able to recieve and send json file to screen', function(done) {
+    chai.request('localhost:3000')
+      .get('/note_one')
+      .end(function(err, res) {
+        expect(err).to.eql(null);
+        expect(res).to.be.json;
+        done();
+      }.bind(this));
+  });
+
   it('should be able to recieve and send json file to screen', function(done) {
     chai.request('localhost:3000')
       .get('/sample')
@@ -19,4 +29,5 @@ describe('The server file', function() {
         done();
       }.bind(this));
   });
+
 });
