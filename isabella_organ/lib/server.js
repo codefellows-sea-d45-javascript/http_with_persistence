d8855790edb023c33a2277c7a87428d5f8037565
Isabella Organ <isabellaorgan@gmail.com>
2015-11-11 07:11:49 -0800

    changes to server, update tests

--- isabella_organ/lib/server.js
+++ isabella_organ/lib/server.js
@@ -2,37 +2,18 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 
-var processData = function(req, res, next) {
-	console.log('process data');
-	var data = '';
-	req.on('data', function(reqData) {
-		data = data + reqData.toString();
-	});
-
-	req.on('end', function(endData) {
-		req.body = data;
-		next();
-	});
-};
-
-app.get('/data/:name', function(req, res) {
-	fs.readFile(__dirname + '/data/' + req.params.name + '.json', function(err, data) {
-		res.json({msg: 'hello world' + req.params.name});
-	});
-	console.log('read data');
+app.get('/data/', function(req, res) {
+	var readStream = fs.createReadStream(__dirname + '/data/post.json');
+		res.writeHead(200, {'content-type':'application.json'});
+		readStream.pipe(res);
 });
 
-app.use(processData);
-
-app.post('/data/:name', function(req, res) {
-	fs.writeFile(__dirname + '/data/' + req.params.name + '.json', req.body, function(err) {
-		if (err) console.log(err);
-		res.send('data posted to new file');
-	});
-});
-
-app.get('/*', function(req, res) {
-	res.status(404).json({msg: 'not found'});
+app.post('/data/', function(req, res) {
+	var writeStream = fs.createWriteStream(__dirname + '/data/post.json');
+		req.pipe(writeStream);
+		req.on('end', function() {
+			res.json({'msg':'hello world'});
+		});
 });
 
 app.listen(3000, function() {
--- isabella_organ/test/test.js
+++ isabella_organ/test/test.js
@@ -13,19 +13,17 @@ describe('the http server', function() {
 		.get('/data/')
 		.end(function(err, res) {
 			expect(err).to.eql(null);
-			// expect(res).to.have.status(200);
-			expect(res.text).to.eql('{"msg":"not found"}');
+			expect(res.body).to.eql('{"msg":"get data"}');
 			done();
 		});
 	});
 	it('should POST to a new file', function(done) {
 		chai.request('localhost:3000')
-			.post('/data/post')
+			.post('/data/post.json')
 			.send('{"msg":"hello world"}')
 			.end(function(err, res) {
 				expect(err).to.eql(null);
-				// expect(res).to.have.status(200);
-				expect(res.text).to.eql("data posted to new file");
+				expect(res.body).to.eql('{"msg":"hello world"}');
 				done();
 			});
 	});
