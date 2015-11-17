b793978f0c8954fefcd9954451cea98b5f22f168
Craig Campbell <craig.campbell8@gmail.com>
2015-11-16 22:10:56 -0800

    rewrote to allow for multiple save files

--- Craig_Campbell/test/server-test.js
+++ Craig_Campbell/test/server-test.js
@@ -35,13 +35,13 @@ describe('the server', function(){
   it('should respond to to POST requests to a valid route by saving the data to a file', function(done){
     var json = {"key": "value"};
     chai.request('localhost:3000')
-    .post('/ok_route')
+    .post('/ok/something')
     .send(json)
     .end(function(err, res){
         expect(err).to.eql(null);
         expect(res.status).to.eql(200);
         expect(res.text).to.eql('We got your request');
-        var filebuffer = fs.readFileSync(__dirname + '/../data/ok_route.json');
+        var filebuffer = fs.readFileSync(__dirname + '/../data/something.json');
         expect(filebuffer.toString()).to.eql(JSON.stringify(json));
       done();
     });
@@ -52,7 +52,7 @@ describe('the server', function(){
   var json = {"key": "value"};
   it('should respond to a GET requests to a valid route by sending any data put there perviously by a POST request', function(done){
     chai.request('localhost:3000')
-    .get('/ok_route')
+    .get('/ok/something')
     .end(function(err, res){
       fs.readFile(__dirname + '../data/something.json', function(){
         expect(err).to.eql(null);
