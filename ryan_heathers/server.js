f66146605550c95c5ddb21fdd79e63fdf8ed433a
Ryan Heathers <ryan@ryanheathers.com>
2015-11-24 23:44:04 -0800

    allow server to respond to wildcard filenames

--- ryan_heathers/server.js
+++ ryan_heathers/server.js
@@ -13,23 +13,25 @@ app.use(function(req, res, next) {
   });
 });
 
-app.post('/kombucha', function(req, res) {
+app.post('/data/:filename', function(req, res) {
   var data = JSON.stringify(req.body);
-  fs.writeFile(__dirname + '/data/kombucha.json', data, function(err) {
+  fs.writeFile(__dirname + '/data/' + req.params.filename, data, function(err) {
     if (err) throw err;
+
     console.log('File saved successfully');
     res.send('It worked!');
   });
 });
 
-app.get('/kombucha', function(req, res) {
-  fs.readFile(__dirname + '/data/kombucha.json', 'utf8', function(err, data) {
+app.get('/data/:filename', function(req, res) {
+  fs.readFile(__dirname + '/data/' + req.params.filename, 'utf8', function(err, data) {
     if (err) throw err;
+
     var json = JSON.parse(data);
     res.send(json);
   });
 });
 
-app.listen(8080, function() {
+app.listen(3000, function() {
   console.log('Server running');
 });
