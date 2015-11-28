79598d9dddbd1429deaa6f594261b9fc86f27b8f
Kenji Crosland <kenji.crosland@gmail.com>
2015-11-28 10:37:30 -0800

    Made changes based on comments

--- kenji_crosland/index.js
+++ kenji_crosland/index.js
@@ -4,34 +4,19 @@ var fs = require('fs');
 var bodyparser = require('body-parser');
 var jsonParser = bodyparser.json();
 
-app.get('/notes', function(req, res) {
-  fs.readFile(__dirname + '/data/notes.json', function(err, data){
+app.get('/:filename', function(req, res) {
+  fs.readFile(__dirname + '/data/' + req.params.filename + '.json', function(err, data){
     var myObj = JSON.parse(data.toString());
     res.json(myObj);
   });
 });
 
-app.post('/notes',jsonParser, function(req, res){
+app.post('/:filename',jsonParser, function(req, res){
   var postString = JSON.stringify(req.body);
-    fs.readFile(__dirname + '/data/notes.json', function(err, data){
-      var returnjson;
-      if(!err && data.toString().length > 0){
-        var fileString = JSON.parse(data.toString());
-        var myArray = fileString.notes;
-        for (var i = 0; i < myArray.length; i++){
-          myArray[i] = JSON.stringify(myArray[i]);
-        }
-        myArray.push(postString);
-        var myString = myArray.join(', ');
-          returnjson = '{"notes":[' + myString + ']}';
-        } else {
-          returnjson = '{"notes":[' + postString + ']}';
-        }
-      fs.writeFile(__dirname + '/data/notes.json', returnjson, function(err){
-        if (err) throw err;
-        res.send(req.body);
+  fs.writeFile(__dirname + '/data/' + req.params.filename + '.json', postString, function(err){
+      if (err) throw err;
+      res.send(req.body);
     });
-  });
 });
 
 app.listen(3000, function(){
