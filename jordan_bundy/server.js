489bc3ec478b68782ae355043ae7f0bb416f2566
Jordan Bundy <jordanbundy@MacBook-Air-5.local>
2015-12-01 12:21:44 -0800

    fixed assignment

--- jordan_bundy/server.js
+++ jordan_bundy/server.js
@@ -2,14 +2,14 @@ var http = require('http');
 var app = require('express')();
 var fs = require('fs');
 
-app.get('/data', function(req, res) {
-  var readStream = fs.createReadStream(__dirname + '/data/input.json');
+app.get('/data/:name', function(req, res) {
+  var readStream = fs.createReadStream(__dirname + '/data/' + req.params.name + '.json');
   res.writeHead(200, {'content-type':'application/json'});
   readStream.pipe(res);
 });
 
-app.post('/data', function(req, res) {
-  var writeStream = fs.createWriteStream(__dirname + '/data/input.json');
+app.post('/data/:name', function(req, res) {
+  var writeStream = fs.createWriteStream(__dirname + '/data/' + req.params.name + '.json');
 
   req.pipe(writeStream);
   req.on('end', function() {
